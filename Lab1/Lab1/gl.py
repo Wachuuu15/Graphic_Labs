3fbd3f76e22fb1583118e10703e86b5dbe8d6af5
wachuuu <88355602+Wachuuu15@users.noreply.github.com>
2023-08-12 21:04:28 -0600

    create

--- Lab1/Lab1/gl.py
+++ Lab1/Lab1/gl.py
@@ -2,7 +2,8 @@ import struct
 from collections import namedtuple
 from obj import Obj
 from numpi import Numpi
-
+from texture import Texture
+import math
 
 V2= namedtuple('Point2', ['x', 'y'])
 V3= namedtuple('Point', ['x', 'y', 'z'])
@@ -35,13 +36,17 @@ class Model(object):
         model= Obj(filename)
 
         self.vertices= model.vertices
-        self.textcoords= model.texcoords
+        self.texcoords = model.texcoords
         self.normals= model.normals
         self.faces= model.faces
 
         self.translate= translate
         self.rotate= rotate
         self.scale= scale
+
+            
+    def LoadTexture(self, textureName):
+        self.texture = Texture(textureName)
 
 class Renderer(object):
     def __init__(self, width, height):
@@ -61,22 +66,33 @@ class Renderer(object):
         self.primitiveType = TRIANGLES
         self.vertexBuffer = []
 
+
+        self.activetexture = None
+
     def glAddVertices(self, vertx):
         for vert in vertx:
             self.vertexBuffer.append(vert)
 
-    def glPrimitiveAssembly(self, tVerts):
+    
+    def glPrimitiveAssemly(self, tVerts, tTextCoords):
         primitives = []
-
         if self.primitiveType == TRIANGLES:
             for i in range(0, len(tVerts), 3):
                 triangle = []
-                triangle.append( tVerts[i] )
-                triangle.append( tVerts[i + 1])
-                triangle.append( tVerts[i + 2])
-                primitives.append(triangle)
+                #vertices
+                triangle.append(tVerts[i])
+                triangle.append(tVerts[i+1])
+                triangle.append(tVerts[i+2])
+                #texturas
+                triangle.append(tTextCoords[i])
+                triangle.append(tTextCoords[i+1])
+                triangle.append(tTextCoords[i+2])
 
+                primitives.append(triangle)
+            
+        
         return primitives
+
 
     def draw_lines(self, verti):
         i = 0
@@ -96,59 +112,93 @@ class Renderer(object):
     def glClear(self):
         self.pixels = [[self.clearColor for y in range(self.height)]
                        for x in range(self.width)]
+        
+        self.zbuffer = [[float('inf') for y in range(self.height)]
+                       for x in range(self.width)]
 
     def glPoint(self, x, y, clr = None):
         if(0 <= x < self.width) and (0 <= y < self.height):
             self.pixels[x][y] = clr or self.currColor
 
-    
+    def glTriangle_bc(self, A, B, C, vtA, vtB, vtC):
+        minX = round(min(A[0], B[0], C[0]))
+        maxX = round(max(A[0], B[0], C[0]))
+        minY = round(min(A[1], B[1], C[1]))
+        maxY = round(max(A[1], B[1], C[1]))
 
-    def fill_polygon(self, vertices, point_to_skip):
-        if len(vertices) < 3:
-            return
+        colorA = (1,0,0)
+        colorB = (0,1,0)
+        colorC = (0,0,1)
 
-        # Encuentra los límites de la caja que contiene el polígono.
-        min_x = min(vertices, key=lambda v: v.x).x
-        max_x = max(vertices, key=lambda v: v.x).x
-        min_y = min(vertices, key=lambda v: v.y).y
-        max_y = max(vertices, key=lambda v: v.y).y
+        for x in range(minX, maxX + 1):
+            for y in range(minY, maxY + 1):
+                P = (x,y)
+                
+                try :
+                    u,v,w = Numpi.barycentricCoords(A,B,C,P)
+                    if 0<=u<=1 and 0<=v<=1 and 0<=w<=1: 
 
-        # Itera sobre cada punto (x, y) dentro de la caja del polígono
-        for x in range(min_x, max_x + 1):
-            for y in range(min_y, max_y + 1):
-                if self.is_point_inside_polygon(x, y, vertices) and (x, y) != point_to_skip:
-                    self.glPoint(x, y, color(1, 0.5, 0.5))
+                        z = u * A[2] + v * B[2] + w * C[2]
 
+                        if z < self.zbuffer[x][y]:
+                            self.zbuffer[x][y] = z
 
+                            uvs = (u * vtA[0] + v * vtB[0] + w * vtC[0],
+                                   u * vtA[1] + v * vtB[1] + w * vtC[1]
+                                  )
 
-    def is_point_inside_polygon(self, x, y, vertices):
-        n = len(vertices)
-        inside = False
-        p1x, p1y = vertices[0]
-        for i in range(n + 1):
-            p2x, p2y = vertices[i % n]
-            if y > min(p1y, p2y):
-                if y <= max(p1y, p2y):
-                    if x <= max(p1x, p2x):
-                        if p1y != p2y:
-                            xinters = (y - p1y) * (p2x - p1x) / (p2y - p1y) + p1x
-                        if p1x == p2x or x <= xinters:
-                            inside = not inside
-            p1x, p1y = p2x, p2y
-        return inside
+                            if self.fragmentShader != None:
+                                colorP = self.fragmentShader(textCoords = uvs, 
+                                                             texture = self.activetexture)
+                                self.glPoint(x, y, color(colorP[0], colorP[1], colorP[2]))
+                            else:
+                                self.glPoint(x, y, colorP)
+                except:
+                    pass
 
 
-    def glLine(self, v0, v1, clr= None):
-        #Bresenham line algorith
-        #y= mx + b
+
+    def glModelMatrix(self, translate = (0,0,0), scale =(1,1,1), rotate=(0,0,0)):
+        #np.matrix
+        translation = [[1,0,0,translate[0]],
+                      [0,1,0,translate[1]],
+                      [0,0,1,translate[2]],
+                      [0,0,0,1]]
         
-        """ m= (v1.y - v0.y) / (v1.x - v0.x)
-        y= v0.y
+        scaleMat = [[scale[0],0,0,0],
+                   [0,scale[1],0,0],
+                   [0,0,scale[2],0],
+                   [0,0,0,1]]
+        
+        pitch = rotate[0] * math.pi/180
+        yaw = rotate[1] * math.pi/180
+        roll = rotate[2] * math.pi/180
 
-        for x in range(v0.x, v1.x + 1):
-            self.glPoint(x, int(y))
-            y += m """
-            
+        pitchMath = [[1,0,0,0],
+            [0,math.cos(pitch),-math.sin(pitch),0 ],
+            [0, math.sin(pitch), math.cos(pitch),0],
+            [0,0,0,1]]
+        
+        yawMath =[[math.cos(yaw),0,math.sin(yaw),0],
+            [0,1,0,0],
+            [-math.sin(yaw),0,math.cos(yaw),0],
+            [0,0,0,1]]
+        
+        rollMath =[[math.cos(roll),-math.sin(roll),0,0],
+            [math.sin(roll),math.cos(roll),0,0],
+            [0,0,1,0],
+            [0,0,0,1]]
+        
+
+        Rxy = Numpi.multMatrices(pitchMath,yawMath)
+        MatrixRot = Numpi.multMatrices(Rxy, rollMath)
+        Mtr = Numpi.multMatrices(translation, MatrixRot)
+        result = Numpi.multMatrices(Mtr, scaleMat)
+        
+        return result #translation * scaleMat#multiplicación matriz 4 * 4
+    
+    def glLine(self, v0, v1, clr= None):
+ 
         x0 = int(v0[0])
         x1 = int(v1[0])        
         y0 = int(v0[1])
@@ -201,78 +251,66 @@ class Renderer(object):
 
                 limit += 1 
     
-    def glLoadModule(self, filename, translate= (0,0,0), rotate=(0,0,0), scale= (1,1,1)):
-        self.objects.append(Model(filename, translate, rotate, scale))
+    def glLoadModule(self, filename, textureName, translate = (0,0,0),rotate = (0,0,0), scale = (1,1,1)):
+        model = Model(filename, translate, rotate, scale)
+        model.LoadTexture(textureName)
+        
+        self.objects.append(model)
 
     def glRender(self):
         transformedVerts = []
-        
+        textCoords = []
+
         for model in self.objects:
-            modelMatrix= self.glModelMatrix(model.translate, model.scale)
+            
+            self.activetexture= model.texture
+
+            mMat = self.glModelMatrix(model.translate, model.scale, model.rotate)
 
             for face in model.faces:
-                vertCount= len(face)
+                verCount = len(face)
 
-                v0= model.vertices[face[0][0] - 1]
-                v1= model.vertices[face[1][0] - 1]
-                v2= model.vertices[face[2][0] - 1]
-
-                if vertCount==4:
-                    v3= model.vertices[face[3][0] - 1]
+                v0 = model.vertices[ face[0][0]-1]
+                v1 = model.vertices[ face[1][0]-1]
+                v2 = model.vertices[ face[2][0]-1]
+                if verCount == 4:
+                    v3 = model.vertices[ face[3][0]-1]
 
                 if self.vertexShader:
-                    v0= self.vertexShader(v0, modelMatrix= modelMatrix)
-                    v1= self.vertexShader(v1, modelMatrix= modelMatrix)
-                    v2= self.vertexShader(v2, modelMatrix= modelMatrix)
-
-                    if vertCount==4:
-                        v3= self.vertexShader(v3, modelMatrix= modelMatrix)
+                    v0 = self.vertexShader(v0, modelMatrix = mMat)
+                    v1 = self.vertexShader(v1, modelMatrix = mMat)
+                    v2 = self.vertexShader(v2, modelMatrix = mMat)
+                    if verCount == 4:
+                        v3 = self.vertexShader(v3, modelMatrix = mMat)
 
                 transformedVerts.append(v0)
                 transformedVerts.append(v1)
                 transformedVerts.append(v2)
-
-                if vertCount==4:
-                    transformedVerts.append(v3)
-                    transformedVerts.append(v1)
+                if verCount == 4:
+                    transformedVerts.append(v0)
                     transformedVerts.append(v2)
+                    transformedVerts.append(v3)
+                
+                vt0 = model.texcoords[face[0][1]-1]
+                vt1 = model.texcoords[face[1][1]-1]
+                vt2 = model.texcoords[face[2][1]-1]
+                if verCount == 4:
+                    vt3 =  model.texcoords[face[3][1]-1]
+                textCoords.append(vt0)
+                textCoords.append(vt1)
+                textCoords.append(vt2)
+                if verCount == 4:
+                    textCoords.append(vt0)
+                    textCoords.append(vt2)
+                    textCoords.append(vt3)
 
-        primitive= self.glPrimitiveAssembly(transformedVerts)
+        primitives = self.glPrimitiveAssemly(transformedVerts, textCoords)       
 
-        primColor= None
-
-        if self.fragmentShader:
-            primColor= self.fragmentShader()
-            primColor = color(primColor[0],
-                              primColor[1],
-                              primColor[2])
-
-        else:
-            primColor= self.currColor
-
-        for prim in primitive:
+        for prim in primitives: 
             if self.primitiveType == TRIANGLES:
-                self.glTriangle(prim[0], prim[1], prim[2], primColor)
+                self.glTriangle_bc(prim[0], prim[1], prim[2], 
+                                   prim[3],prim[4],prim[5])
 
-    def glTriangle(self, v0, v1, v2, clr= None):
-        self.glLine(v0, v1, clr or self.currColor)
-        self.glLine(v1, v2, clr or self.currColor)
-        self.glLine(v2, v0, clr or self.currColor)
-
-    def glModelMatrix(self, translate=(0,0,0), scale=(1,1,1)):
-        translation = ([[1, 0, 0, translate[0]],
-                       [0, 1, 0, translate[1]],
-                       [0, 0, 1, translate[2]],
-                       [0, 0, 0, 1]])
-
-        scaleMat = ([[scale[0], 0, 0, 0],
-                    [0, scale[1], 0, 0],
-                    [0, 0, scale[2], 0],
-                    [0, 0, 0, 1]])
-        
-        multMatrix = Numpi.multMatrices(translation, scaleMat)  # Añade numpi() para crear una instancia de la clase.
-        
-        return multMatrix
 
         
     def glFinish(self, filename):
